41d24da3f5e9661a035c582f73c1bec4ebfdd481
82deutschmark <38813642-82deutschmark@users.noreply.replit.com>
2025-03-05 17:06:39 +0000

    Restored to 'bfdc18027b550c262258609a4c34103c2789f3cf'
    Replit-Restored-To: bfdc18027b550c262258609a4c34103c2789f3cf

--- models.py
+++
@@ -1,8 +0,0 @@
-from app import db
-
-class Hashtag(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(50), unique=True, nullable=False)
-
-    def __repr__(self):
-        return f'<Hashtag {self.text}>'
